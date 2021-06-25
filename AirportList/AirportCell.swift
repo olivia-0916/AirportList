5b828a85d638855c070fe3199220d66ba1e0cfa6
olivia-0916 <yuxuanlin599@gmail.com>
2021-06-25 15:41:02 +0800

    up

--- AirportList/AirportCell.swift
+++ AirportList/AirportCell.swift
@@ -13,9 +13,11 @@ class AirportCell: UITableViewCell {
     @IBOutlet weak var namelabel: UILabel!
     @IBOutlet weak var countrylabel: UILabel!
     
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
+    
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
