8ff60e7f5a80af8482bcbc3f370f68a574a8c8c2
olivia-0916 <yuxuanlin599@gmail.com>
2021-06-25 14:27:49 +0800

    update label

--- AirportList/DetailPage.swift
+++ AirportList/DetailPage.swift
@@ -15,16 +15,17 @@ class DetailPage: UIViewController {
     
     @IBOutlet weak var ITATlabel: UILabel!
     
+    @IBOutlet weak var shortname: UILabel!
     var airport: Airport?
-    
-    
+    @IBOutlet weak var servedcity: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         photoimage.image = UIImage(named: (airport?.Image)!)
         countrylabel.text = airport?.Country
         namelabel.text = airport?.Airport
         ITATlabel.text = airport?.IATA
-        
+        shortname.text = airport?.ShortName
+        servedcity.text = airport?.ServedCity
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
